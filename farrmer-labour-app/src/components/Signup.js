fe20ede34e6e7d2cd9111afcc6d3c2c8d75eb023
maldanna <maldanna432@gmail.com>
2023-01-02 13:36:21 +0000

    Able to call spring boot application

--- farrmer-labour-app/src/components/Signup.js
+++ farrmer-labour-app/src/components/Signup.js
@@ -1,56 +1,76 @@
 import React, { useState } from "react";
-import HomePage from "./HomePage";
-import { Modal,Button } from "react-bootstrap";
-import createPortal from 'react-dom';
-import SignupModal from './SignupModal';
-import "./SignupStyle.css"
+import { useNavigate } from 'react-router-dom';
+import "./SignupStyle.css";
+import { SpringBootURLData } from "./SpringBootURLData";
 function Signup()
 {   
-    const [signupData,setsignupData] =useState({});
+    const [signupData,setsignupData] =useState({ name: '', email: '' ,phone: '',password: ''});
     const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
+    const [password, setPass] = useState('');
     const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
     
-    const [show, setShow] = useState(true);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setsignupData({
-            "email":"maldanna432@gmail.com",
-            "password":"1234"
-        });
-        alert("done");
-        console(signupData["email"]);
-        console(signupData["password"]);
-        setEmail(e.email.value)
+    const handleChange = (event) => {
+        const { name, value } = event.target
+        setsignupData({ ...signupData, [name]: value })
+
+      };
+      /* submit form to spring boot application */
+    const signupFormSubmit = (event) => {
+        event.preventDefault();
+        console.log(signupData);
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        //myHeaders.append("Access-Control-Allow-Origin", "*"); myHeaders.append("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT");myHeaders.append("Access-Control-Allow-Headers", "X-Token");
+        var raw = JSON.stringify(signupData);
+        var requestOptions = { method: SpringBootURLData.register.method,headers: myHeaders,body: raw };
+        fetch(SpringBootURLData.register.url, requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
     }
-    const signupFormSubmit = () =>{
-        alert("form submitted");
+
+    function handleCancel() {
+        navigate('/');
     }
+
     return (
-        <div class="signup-form">
+        <div className="signup-form">
             <form onSubmit={signupFormSubmit}>
                 <h3>Sign Up</h3>
                 <div className="mb-3">
-                    <label>Phone Number</label>
-                    <input type="tel" class="form-control" name="phone" placeholder="1234567890" pattern="[0-9]{3} [0-9]{3} [0-9]{4}" maxlength="10"  required/>    
+                    <label>Name</label>
+                    <input type="text" className="form-control" name="name" onChange={handleChange} placeholder="Enter Your Name" maxlength="50"  required/>    
                 </div>
                 <div className="mb-3">
+                    <label>Phone Number</label>
+                    <input type="tel" className="form-control" name="phone" onChange={handleChange}  placeholder="1234567890" maxLength="10"  required/>    
+                </div>
+
+                <div className="mb-3">
+                    <label>Email</label>
+                    <input type="email" className="form-control" name="email" onChange={handleChange}  placeholder="abc@gmail.com" maxLength="50"  required/>    
+                </div>
+
+                <div className="mb-3">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" required />
+                    <input type="password" className="form-control" name="password" onChange={handleChange}  placeholder="Enter password" required />
                 </div>
                 <div className="d-grid">
-                <button type="submit" className="btn btn-primary" > Create Account </button>
+                <div className="row">
+                    <div className="col-3"> <button type="submit" className="btn btn-primary" >Submit</button></div>
+                    <div className="col-3"> <button type="button" className="btn btn-danger" onClick={handleCancel} >Cancel</button></div>
+                </div>
+
                 </div>
                 <p className="forgot-password text-right">
-                Already registered <a href="/sign-in">sign in?</a>
+                Already registered <a href="/login">sign in?</a>
                 </p>
             </form>
         </div>
         );
  }
 export default Signup;
-    // used below link for react input validation
-    //https://www.npmjs.com/package/reactjs-input-validator
+
---
+++ farrmer-labour-app/src/components/SpringBootURLData.js
@@ -0,0 +1,18 @@
+export const SpringBootURLData={
+    
+    register: {
+        url:"https://8080-maldanna-springangularf-dibqneu78m4.ws-us80.gitpod.io/user/register",
+        method:"POST"
+    },
+
+    login: {
+        url:"",
+        method:"POST"
+    },
+
+    updateUser: {
+        url:"",
+        method:"POST"
+    }
+}
+
